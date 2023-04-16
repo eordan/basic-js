2558bd1f3bf808cb30d1faefed0e0d306f36f0d9
Андрей Писцов <pistsouandrey@gmail.com>
2023-04-16 11:49:45 +0300

    feat: solve sort-by-height & vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -19,14 +19,73 @@ const { NotImplementedError } = require('../extensions/index.js');
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
  * 
  */
+
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  constructor(isDirect = true) {
+    this.isDirect = isDirect;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
+
+  encrypt(message, key) {
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+
+    if (!message || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+
+    let result = '';
+    let j = 0;
+
+    for (let i = 0; i < message.length; i++) {
+      const charCode = message.charCodeAt(i);
+
+      if (charCode >= 65 && charCode <= 90) {
+        const keyChar = key.charCodeAt(j % key.length) - 65;
+        const encryptedCharCode = ((charCode - 65 + keyChar) % 26) + 65;
+
+        result += String.fromCharCode(encryptedCharCode);
+        j++;
+      } else {
+        result += message[i];
+      }
+    }
+
+    return this.isDirect ? result : result.split('').reverse().join('');
+  }
+
+  decrypt(encryptedMessage, key) {
+    // throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
+
+    if (!encryptedMessage || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    encryptedMessage = encryptedMessage.toUpperCase();
+    key = key.toUpperCase();
+
+    let result = '';
+    let j = 0;
+
+    for (let i = 0; i < encryptedMessage.length; i++) {
+      const charCode = encryptedMessage.charCodeAt(i);
+
+      if (charCode >= 65 && charCode <= 90) {
+        const keyChar = key.charCodeAt(j % key.length) - 65;
+        const decryptedCharCode = ((charCode - 65 - keyChar + 26) % 26) + 65;
+
+        result += String.fromCharCode(decryptedCharCode);
+        j++;
+      } else {
+        result += encryptedMessage[i];
+      }
+    }
+
+    return this.isDirect ? result : result.split('').reverse().join('');
   }
 }
 
